507ab39e926fab47f9b8d5d97748b38183950caf
Brandon Villa <bornofos@gmail.com>
2020-06-23 22:01:09 -0500

    Initial exercice

---
+++ js/index.js
@@ -0,0 +1,13 @@
+/**
+ * 1. Agregar el texto "facebook" al header h1
+ * 
+ * 2. Crear un boton de login y agregarlo en donde se indica en el documento HTML
+ * 
+ * 3. Agregar un evento que al momento de realizar un Submit, se ejecute y muestre
+ *    en un alert el email y password que se ingresaron
+ * 3.1 Obtener el email y password navegando a traves de los nodos hijos del loginForm
+ * 
+ * 4. Agregar un evento que al momento de realizar un click en el boton de Registro
+ *    se muestre un alert con los datos ingresados.
+ * 4.1 Obtener los valores de los inputs a traves de su id
+ */
