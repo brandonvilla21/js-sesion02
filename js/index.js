4c3120786ed1975973acada715eca2fe755aace9
Brandon Villa <bornofos@gmail.com>
2020-06-29 22:05:13 -0500

    Cambios en clase

--- js/index.js
+++ js/index.js
@@ -8,7 +8,40 @@
  *    en un alert el email y password que se ingresaron
  * 3.1 Obtener el email y password navegando a traves de los nodos hijos del loginForm
  * 
- * 4. Agregar un evento que al momento de realizar un click en el boton de Registro
+ * 4. (Ejercicio de tarea) Agregar un evento que al momento de realizar un click en el boton de Registro
  *    se muestre un alert con los datos ingresados.
  * 4.1 Obtener los valores de los inputs a traves de su id
+ * 
+ * 5. (Ejercicio de tarea) En el Login, al hacer el submit, verificar que si el password esta vacio,
+ * entonces muestra un alert indicando al usuario que la contraseña no es valida
  */
+
+
+// Obtener el elemento H1
+// Agregar el texto al elemento
+var titleHeader = document.getElementById('facebook-title-h1')
+titleHeader.innerHTML = "Facebook"
+
+// Crear boton de login
+var button = document.createElement('button')
+button.setAttribute('class', 'login-button')
+
+// createTextNode()
+var buttonText = document.createTextNode('Iniciar Sesión')
+button.appendChild(buttonText)
+
+// .appendChild()
+var loginForm = document.getElementById('login-form-id')
+loginForm.appendChild(button)
+
+// Agregar un event listener al botton de login
+function submitForm(event) {
+  event.preventDefault()
+  var arrayChildren = loginForm.children
+  var email = arrayChildren[0].children[1].value
+  var password = arrayChildren[1].children[1].value
+  var message = email + ' - ' + password
+  alert(message)
+}
+
+loginForm.addEventListener('submit', submitForm)
